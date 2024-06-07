a7c5f1a645052bc80e491aad22335f75b80ffb3f
Lucas Purkota <lucaspurkota@gmail.com>
2024-06-07 19:51:56 -0300

    atualização Swagger

--- src/routes/UsuarioRoutes.ts
+++ src/routes/UsuarioRoutes.ts
@@ -10,4 +10,16 @@ router.post("/create", async (req: Request, res: Response) => {
   return res.status(response === "OK" ? 200 : 400).send(response)
 })
 
+router.patch("/update", async (req: Request, res: Response) => {
+  const response = await controller.update(req.body)
+
+  return res.status(response.error ? 400 : 200).send(response)
+})
+
+router.delete("/delete/:id", async (req: Request, res: Response) => {
+  const response = await controller.delete(req.params.id)
+
+  return res.status(response.error ? 400 : 200).send(response)
+})
+
 export default router
